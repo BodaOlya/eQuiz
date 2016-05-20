4d4f872286963c926e83811f1014b39fc57df29f
Maryan Mayher <mr.maykher@gmail.com>
2016-05-20 23:14:12 +0300

    Added classes QuizResult and UserAnswer to keep tabs on result of quiz

---
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Models/QuizResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eQuiz.Web.Areas.Student.Models
+{
+    public class QuizResultModel
+    {
+        public int QuizId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime FinishDateTime { get; set; }
+        public List<UserAnswer> UserAnswers { get; set; }
+    }
+}
