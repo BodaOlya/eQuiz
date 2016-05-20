0a1457aa983311e53f826c110d4d309734e1fc3e
Volodymyr Voityshyn <voytyshyn@gmail.com>
2016-05-20 19:52:30 +0300

    Fixed unit tests (after data access infrastructure integration)

--- eQuiz/modules/eQuiz.Web/Areas/Student/Controllers/DefaultController.cs
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Controllers/DefaultController.cs
@@ -21,11 +21,9 @@ namespace eQuiz.Web.Areas.Student.Controllers
 
         #region Constructors
 
-        public DefaultController()
+        public DefaultController(IRepository repository)
         {
-            var dataContextSettings = new DefaultDataContextSettings(System.Configuration.ConfigurationManager.ConnectionStrings["eQuizDB"].ConnectionString);
-            var dataContextFactory = new EFDataContextFactory(dataContextSettings);
-            this._repository = new EFRepository(dataContextFactory);
+            this._repository = repository;
         }
         #endregion
 
--- eQuiz/tests/eQuiz.Web.Tests/Areas/Moderator/Controllers/DefaultControllerTest.cs
+++ eQuiz/tests/eQuiz.Web.Tests/Areas/Moderator/Controllers/DefaultControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using eQuiz.Repositories.Abstract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +19,8 @@ namespace eQuiz.Web.Tests.Areas.Moderator.Controllers
         [TestInitialize]
         public void BeforeTestMethod()
         {
-            _controller = new ModeratorDefaultController();
+            var repositoryMock = new Mock<IRepository>();
+            _controller = new ModeratorDefaultController(repositoryMock.Object);
         }
 
         [TestCleanup]
