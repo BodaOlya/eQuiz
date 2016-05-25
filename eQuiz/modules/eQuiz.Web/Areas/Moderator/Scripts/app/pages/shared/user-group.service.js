c3e23062b1b6b3ddae6e6f0957f0517f92875e2f
Oleks Sakharuk <ol.sakharuk@gmail.com>
2016-05-25 15:50:48 +0300

    Moved UserGroup.service.cs to shared and rename it to user-group.service.js

---
+++ eQuiz/modules/eQuiz.Web/Areas/Moderator/Scripts/app/pages/shared/user-group.service.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    angular.module('equizModule')
+           .factory('userGroupService', userGroupService)
+    userGroupService.$inject = ['$http'];
+
+    function userGroupService($http) {
+
+        return {
+            get: get,
+            getGroup: getGroup
+        };
+
+        function get() {
+            return $http.get('/moderator/usergroup/get');
+        }
+
+        function getGroup(id) {
+            var promise = $http.get("/moderator/usergroup/getusergroup?id=" + id.toString());
+            promise.then(populateResponse);
+            return promise;
+        };
+
+        function populateResponse(response) {
+            return response.data;
+        };
+    };
+})();
