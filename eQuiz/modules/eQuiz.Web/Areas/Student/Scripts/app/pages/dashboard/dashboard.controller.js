843fd0caef2132a1098848359985ab019e308cd2
Mayher Maryan <mr.maykher@gmail.com>
2016-05-26 17:33:00 +0300

    Added loading message indicator while loading list of all quizzes

--- eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/app/pages/dashboard/dashboard.controller.js
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/app/pages/dashboard/dashboard.controller.js
@@ -5,11 +5,14 @@
     equizModule.controller("dashboardCtrl", ["$scope", "$http", "dashboardService", function ($scope, $http, dashboardService) {
         $scope.allQuizes = null;
 
+        $scope.isLoading = true;
+
         activate();
         function activate() {
             var quizPromise = dashboardService.getQuizzes();
             quizPromise.success(function (data) {
                 $scope.allQuizes = data;
+                $scope.isLoading = false;
             });
         };
     }]);
