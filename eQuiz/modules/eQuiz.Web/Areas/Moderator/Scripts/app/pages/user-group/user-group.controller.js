d71fd45e28131ddc985ce57a980c4946b61a4667
Oleks Sakharuk <ol.sakharuk@gmail.com>
2016-05-27 16:01:31 +0300

    Added success, error and processing messages

--- eQuiz/modules/eQuiz.Web/Areas/Moderator/Scripts/app/pages/user-group/user-group.controller.js
+++ eQuiz/modules/eQuiz.Web/Areas/Moderator/Scripts/app/pages/user-group/user-group.controller.js
@@ -3,28 +3,37 @@
         .module('equizModule')
         .controller('UserGroupController', UserGroupController);
 
-    UserGroupController.$inject = ['$scope', 'userGroupService', '$location'];
+    UserGroupController.$inject = ['$scope', 'userGroupService', '$location', '$timeout'];
 
-    function UserGroupController($scope, userGroupService, $location) {
+    function UserGroupController($scope, userGroupService, $location, $timeout) {
 
         var vm = this;
         vm.users = [];
         
         vm.predicate = 'LastName';
-        vm.reverse = false;        
+        vm.reverse = false;
+        vm.errorMessageVisible = false;
+        vm.successMessageVisible = false;
+        vm.loadingVisible = false;
 
         vm.sortBy = sortBy;
         vm.showOrderArrow = showOrderArrow;
         vm.deleteUser = deleteUser;
         vm.save = save;
+        vm.showSuccess = showSuccess;
+        vm.showError = showError;
+        vm.showLoading = showLoading;
+        vm.hideLoading = hideLoading;
 
         activate();
 
         function activate() {
             if ($location.search().id) {
+                vm.showLoading();
                 userGroupService.getGroup($location.search().id).then(function (data) {
                     vm.group = data.data.group;
                     vm.users = data.data.users;
+                    vm.hideLoading();
                 });
             }
         };
@@ -32,8 +41,7 @@
 
         function sortBy(predicate) {
             vm.reverse = (vm.predicate === predicate) ? !vm.reverse : false;
-            vm.predicate = predicate;
-            //reloadGroup();
+            vm.predicate = predicate;            
         };
 
         function showOrderArrow(predicate) {
@@ -45,17 +53,41 @@
 
         function deleteUser(user) {
             var userIndex = vm.users.indexOf(user);
-            console.log(user);
-            console.log(userIndex);
             vm.users.splice(userIndex, 1);
         };
 
         function save() {
+            vm.showLoading();
             userGroupService.save({ userGroup: vm.group, users: vm.users }).then(function (data) {
                 vm.group = data.data.group;
-                vm.users = data.data.users
+                vm.users = data.data.users;
+                vm.hideLoading();
+                vm.showSuccess();
+            }, function (data) {
+                vm.hideLoading();
+                vm.showError();
             });
         };
+
+        function showSuccess() {
+            vm.successMessageVisible = true;
+            $timeout(function () {
+                vm.successMessageVisible = false;
+                window.location.href = '/moderator/usergroup/index';
+            }, 2000);
+        }
+        function showError() {
+            vm.errorMessageVisible = true;
+            $timeout(function () {
+                vm.errorMessageVisible = false;
+            }, 4000);
+        }
+        function showLoading() {
+            vm.loadingVisible = true;
+        }
+        function hideLoading() {
+            vm.loadingVisible = false;
+        }
     };
 
 })(angular)
