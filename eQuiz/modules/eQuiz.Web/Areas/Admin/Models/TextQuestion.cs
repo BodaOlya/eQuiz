b95f4e58f82583fae5e56e1ff966337cb8a8d406
Dmytro Maherovskyi <xxmdvxx@gmail.com>
2016-05-31 22:17:34 +0300

    Quiz check data structure development

---
+++ eQuiz/modules/eQuiz.Web/Areas/Admin/Models/TextQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eQuiz.Web.Areas.Admin.Models
+{
+    public class TextQuestion : QuestionBase
+    {
+        public string Answer { get; set; }
+        public string RightAnswer { get; set; }
+
+        public TextQuestion(int id, int maxScore, int userScore, string questionText, string answer, string rightAnswer) : base(id, maxScore, userScore, questionText)
+        {
+            base.Type = "Text";
+            Answer = answer;
+            RightAnswer = answer;
+        }
+    }
+}
