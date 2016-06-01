06de8f20ceab65d62742ed8aee82dd67726f2257
molochiy <molochiy@gmail.com>
2016-06-01 03:00:46 +0300

    Added import-users-from-file directive

---
+++ eQuiz/modules/eQuiz.Web/Areas/Moderator/Scripts/app/pages/user-group/import-users-from-file.directive.js
@@ -0,0 +1,47 @@
+﻿(function (angular) {
+    angular.module('equizModule')        
+        .directive('importUsersFromFile', importUsers);
+
+    function importUsers() {
+        return {
+            scope: {
+                callback: '&'
+            },
+            link: function (scope, el, attrs) {
+                el.bind('change', function (event) {
+                    var files = event.target.files;
+                    var file = files[0];
+
+                    var data = new Array();
+
+                    var reader = new FileReader();
+
+                    reader.onload = function (event) {
+
+                        var contents = event.target.result;
+
+                        var allTextLines = contents.split(/\r\n|\n/);
+
+                        for (var i = 0; i < allTextLines.length; i++) {
+
+                            var line = allTextLines[i].split(',');
+
+                            data.push({
+                                firstname: line[0],
+                                secondname: line[1],
+                                email: line[2]
+                            });
+                        }
+                        
+                        scope.callback({data: data});                        
+                    };
+
+                    reader.readAsText(file);
+                    
+                    scope.$apply();
+                });
+            }
+        };
+    };
+
+})(angular)
