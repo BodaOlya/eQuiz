c8d983726a23628637a5d05c2bdb63e657faf21b
BodaOlya <oliab1904@gmail.com>
2016-06-03 20:04:24 +0300

    unique-uswr-email directive(test)

---
+++ eQuiz/modules/eQuiz.Web/Areas/Moderator/Scripts/app/pages/user-group/unique-user-email.directive.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    angular
+    .module('equizModule')
+    .directive('uniqueUserEmail', uniqueUserEmail);
+    //todo
+    uniqueUserEmail.$inject = ['userGroupService'];
+    function uniqueUserEmail(userGroupService) {
+        return {
+            restrict: 'A',
+            require: '^form',
+            scope: {
+                user: '=',
+                users: '='
+            },
+            link: function (scope, element, attributes, formControl) {
+                var inputElement = element[2].querySelector("[name]");
+                var inputNgElement = angular.element(inputElement);
+                var inputName = inputNgElement.attr('name');
+                var messagesBlock = inputNgElement.next();
+                console.log("IN DIRECTIVE ", scope.users);
+                alert("gfghfh");
+                function callback(data) {
+                    formControl.name.$setValidity('repeatedMessage', data);
+                    element.toggleClass('has-error', formControl[inputName].$invalid);
+                    messagesBlock.toggleClass('hide', formControl[inputName].$valid);
+                }
+
+                inputNgElement.bind('blur', function () {
+                    var isValid = true;
+                    userGroupService.isUsersValid(user).then(function (data) {
+                        isValid = data.data;
+                    });
+                    for (var i = 0; i < users.length; i++) {
+                        if (users[i].Email === user.Email) {
+                            isValid = false;
+                        }
+                        callback(isValid)
+                    }
+
+                })
+
+            }
+        }
+    }
+})
