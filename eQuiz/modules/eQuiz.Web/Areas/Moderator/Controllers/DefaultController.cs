f5f588233469f653d2b09ca9d3479a529039a22d
molochiy <molochiy@gmail.com>
2016-06-09 20:09:49 +0300

    Add info about UserGroup to Moderator Dashboard

--- eQuiz/modules/eQuiz.Web/Areas/Moderator/Controllers/DefaultController.cs
+++ eQuiz/modules/eQuiz.Web/Areas/Moderator/Controllers/DefaultController.cs
@@ -32,26 +32,20 @@ namespace eQuiz.Web.Areas.Moderator.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            //using (var db = new eQuizEntities(System.Configuration.ConfigurationManager.ConnectionStrings["eQuizDB"].ConnectionString))
-            //{
-            //    ViewBag.QuizzesCount = db.Quizs.Count();
-            //    var today = DateTime.Now;
-            //    ViewBag.ActiveQuizzesCount = (from quiz in db.Quizs where (quiz.StartDate <= today && today <= quiz.EndDate) select quiz).Count();
-            //    ViewBag.InactiveQuizzesCount = (from quiz in db.Quizs where quiz.StartDate >= today select quiz).Count();
-            //    ViewBag.QuestionsCount = db.Questions.Count();
-            //    ViewBag.ActiveQuestionsCount = (from question in db.Questions where question.IsActive select question).Count();
-            //    ViewBag.UserGroupsCount = db.UserGroups.Count();
-            //    ViewBag.StudentsCount = db.Users.Count();
-            //};
-
             var today = DateTime.Now; // Todo: implement with ITimeService
             
             ViewBag.QuizzesCount = _repository.Count<Quiz>();
-            ViewBag.ActiveQuizzesCount = _repository.Count<Quiz>(quiz => quiz.StartDate <= today && today <= quiz.EndDate);            
-            ViewBag.InactiveQuizzesCount = _repository.Count<Quiz>(quiz => quiz.StartDate >= today);            
-            ViewBag.QuestionsCount = _repository.Count<Question>();            
-            ViewBag.ActiveQuestionsCount = _repository.Count<Question>(question => question.IsActive);            
-            ViewBag.UserGroupsCount = _repository.Count<UserGroup>();            
+            ViewBag.ActiveQuizzesCount = _repository.Count<Quiz>(quiz => quiz.StartDate <= today && today <= quiz.EndDate);
+            ViewBag.InactiveQuizzesCount = _repository.Count<Quiz>(quiz => quiz.StartDate >= today);
+            ViewBag.QuestionsCount = _repository.Count<Question>();
+            ViewBag.ActiveQuestionsCount = _repository.Count<Question>(question => question.IsActive);
+            ViewBag.UserGroupsCount = _repository.Count<UserGroup>();
+
+            var activeStateId = _repository.GetSingle<UserGroupState>(ugs => ugs.Name == "Active").Id;
+            ViewBag.ActiveUserGroups = _repository.Count<UserGroup>(userGroup => userGroup.UserGroupStateId == activeStateId);
+
+            var archivedStateId = _repository.GetSingle<UserGroupState>(ugs => ugs.Name == "Archived").Id;
+            ViewBag.ArchivedUserGroups = _repository.Count<UserGroup>(userGroup => userGroup.UserGroupStateId == archivedStateId);
             ViewBag.StudentsCount = _repository.Count<User>();
 
             return View();
