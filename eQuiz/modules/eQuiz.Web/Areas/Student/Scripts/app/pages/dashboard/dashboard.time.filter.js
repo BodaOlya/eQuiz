11a8b896c90ee12cefe0bdd2c2c7f253ff457cdb
DOP <workplace0594@ukr.net>
2016-06-25 03:54:04 +0300

    Changed Start date format and Duration time format according to mock up

---
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/app/pages/dashboard/dashboard.time.filter.js
@@ -0,0 +1,16 @@
+﻿(function (angular) {
+    angular
+        .module('equizModule')
+        .filter('timeFromMinutes', customTime);
+
+    function customTime() {
+        return function (inputMinutes) {
+            if (inputMinutes != null || inputMinutes != undefined) {
+                var hours = Math.trunc(inputMinutes / 60);
+                var minutes = inputMinutes % 60;
+
+                return ((hours > 0) ? hours + "h " : "") + ((minutes > 0) ? minutes + "min" : "");
+            }
+        };
+    };
+})(angular);
